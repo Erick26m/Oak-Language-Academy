ffb03b2315ae4da924f2c8b618cda806309679b4
Erick26m <a01715444@tec.mx>
2026-05-26 20:53:48 -0600

    Update membresia.h

--- membresia.h
+++ membresia.h
@@ -8,35 +8,64 @@
 #include <string>
 using namespace std;
 
-class Membresia{
-
+class Membresia {
 private:
-
     string tipo;
     float costo;
     int duracionDias;
 
 public:
+    Membresia();
+    Membresia(string tipo, float costo, int duracionDias);
 
-    Membresia()
-    : tipo("Sin membresia"), costo(0), duracionDias(0){}
+    string getTipo();
+    void setTipo(string tipo);
 
-    Membresia(string tip,float cos,int duracion)
-    : tipo(tip), costo(cos), duracionDias(duracion){}
+    float getCosto();
+    void setCosto(float costo);
 
-    float calcularCosto(){
+    int getDuracionDias();
+    void setDuracionDias(int duracionDias);
 
-        return costo;
-
-    }
-
-    void mostrarMembresia(){
-
-        cout<<"Tipo: "<<tipo<<endl;
-        cout<<"Costo: "<<costo<<endl;
-        cout<<"Duracion: "<<duracionDias<<" dias"<<endl;
-
-    }
-
+    float calcularCosto();
+    string obtenerDescripcion();
 };
+
+Membresia::Membresia()
+    : tipo("Sin membresia"), costo(0), duracionDias(0) {}
+
+Membresia::Membresia(string tipo, float costo, int duracionDias)
+    : tipo(tipo), costo(costo), duracionDias(duracionDias) {}
+
+string Membresia::getTipo() {
+    return tipo;
+}
+
+void Membresia::setTipo(string tipo) {
+    this->tipo = tipo;
+}
+
+float Membresia::getCosto() {
+    return costo;
+}
+
+void Membresia::setCosto(float costo) {
+    this->costo = costo;
+}
+
+int Membresia::getDuracionDias() {
+    return duracionDias;
+}
+
+void Membresia::setDuracionDias(int duracionDias) {
+    this->duracionDias = duracionDias;
+}
+
+float Membresia::calcularCosto() {
+    return costo;
+}
+
+string Membresia::obtenerDescripcion() {
+    return tipo + "$" + to_string(costo) + "  " + to_string(duracionDias) + " dias";
+}
 #endif //UNTITLED4_MEMBRESIA_H
