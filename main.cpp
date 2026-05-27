5252564907c66deafeee832d8b15b8e5e2d7e709
Erick26m <a01715444@tec.mx>
2026-05-26 20:53:29 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,57 @@
 #include "cliente.h"
 #include "entrenador.h"
+#include <iostream>
+using namespace std;
 
-int main(){
+int main() {
 
     Membresia mem1("Premium",850,30);
 
-    Rutina r1("Full Body",
-              "Ganar musculo",
-              60,
-              "Media");
+    Rutina rutina1("Full Body",
+                     "Ganar musculo",
+                     60,
+                     "Media");
 
-    Cliente c1("Armando",
-               19,
-               1,
-               75,
-               1.75,
-               "Musculo",
-               mem1);
+    Cliente cliente1(
+        "Armando",
+        19,
+        101,
+        75.5,
+        1.75,
+        "Ganar musculo",
+        mem1
+    );
 
-    Entrenador e1("Erick",
-                  28,
-                  2,
-                  "Hipertrofia",
-                  5);
+    Entrenador entrenador1(
+        "Erick",
+        30,
+        201,
+        "Hipertrofia",
+        5
+    );
 
-    e1.asignarRutina(c1,r1);
+    entrenador1.asignarRutina(cliente1,rutina1);
 
-    c1.mostrarDatos();
+    // Aqui aplico el polimorfismo:
+    // utilizando un arreglo de punteros de la clase Persona.Aunque el arreglo pertenece a la clase madre,
+    // se almacenan objetos de clases hijas como Cliente yEntrenador
+    // Esto me permite trabajar con distintos tipos de objetos
+    // utilizando una sola referencia. Después, mediante el
+    // metodo virtual obtenerResumen(), el programa identifica
+    // automáticamente el tipo real de cada objeto y ejecuta el metodo correspondiente.
+    // Por ejemplo, cuando el objeto almacenado es un Cliente,
+    // se ejecuta Cliente::obtenerResumen(), y cuando el objeto
+    // es un Entrenador, se ejecuta Entrenador::obtenerResumen().
+    Persona* personas[2];
 
-    cout<<endl;
+    personas[0] = &cliente1;
+    personas[1] = &entrenador1;
 
-    e1.mostrarDatos();
+    for(int i=0;i<2;i++){
+
+        cout<<personas[i]->obtenerResumen()<<endl;
+
+    }
 
     return 0;
 }
