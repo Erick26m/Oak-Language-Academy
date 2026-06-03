7584034992eb83ab93f783212bc1a8d4b0d6a91d
Erick26m <a01715444@tec.mx>
2026-06-03 15:13:33 -0600

    Update profesor.h

--- profesor.h
+++ profesor.h
@@ -1,61 +1,62 @@
 //
 // Created by munoz on 25/05/2026.
 //
-
-#ifndef UNTITLED4_ENTRENADOR_H
-#define UNTITLED4_ENTRENADOR_H
+#ifndef UNTITLED4_PROFESOR_H
+#define UNTITLED4_PROFESOR_H
 #include "persona.h"
-#include "cliente.h"
-#include "rutina.h"
-class Entrenador : public Persona {
+#include "estudiante.h"
+#include "curso.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Profesor : public Persona {
 private:
-    string especialidad;
+    string idioma;
     int aniosExperiencia;
 
 public:
-    Entrenador();
-    Entrenador(string nombre, int edad, int id, string especialidad, int aniosExperiencia);
+    Profesor()
+        : Persona(),
+          idioma("Sin idioma"),
+          aniosExperiencia(0) {}
 
-    string getEspecialidad();
-    void setEspecialidad(string especialidad);
+    Profesor(string nombre, int edad, string matricula,
+             string idioma, int aniosExperiencia)
+        : Persona(nombre, edad, matricula),
+          idioma(idioma),
+          aniosExperiencia(aniosExperiencia) {}
 
-    int getAniosExperiencia();
+    ~Profesor() override = default;
 
-    void asignarRutina(Cliente &cliente, Rutina rutina);
-    void asignarRutina(Cliente &cliente, string nombreRutina, string objetivoRutina);
+    string getIdioma() {
+        return idioma;
+    }
 
-    string obtenerResumen() override;
+    void setIdioma(string idioma) {
+        this->idioma = idioma;
+    }
+
+    int getAniosExperiencia() {
+        return aniosExperiencia;
+    }
+
+    void asignarCurso(Estudiante &estudiante, Curso curso) {
+        estudiante.asignarCurso(curso);
+    }
+
+    // Sobrecarga de metodo
+    void asignarCurso(Estudiante &estudiante, string nombreCurso, string objetivoCurso) {
+        estudiante.asignarCurso(nombreCurso, objetivoCurso);
+    }
+
+    string obtenerResumen() override {
+        return "Profesor: " + nombre +
+               "  Matricula: " + matricula +
+               "  Idioma: " + idioma +
+               "  Experiencia: " + to_string(aniosExperiencia) + " anios";
+    }
 };
 
-Entrenador::Entrenador()
-    : Persona(), especialidad(""), aniosExperiencia(0) {}
-
-Entrenador::Entrenador(string nombre, int edad, int id, string especialidad, int aniosExperiencia)
-    : Persona(nombre, edad, id), especialidad(especialidad), aniosExperiencia(aniosExperiencia) {}
-
-string Entrenador::getEspecialidad() {
-    return especialidad;
-}
-
-void Entrenador::setEspecialidad(string especialidad) {
-    this->especialidad = especialidad;
-}
-
-int Entrenador::getAniosExperiencia() {
-    return aniosExperiencia;
-}
-
-void Entrenador::asignarRutina(Cliente &cliente, Rutina rutina) {
-    cliente.asignarRutina(rutina);
-}
-
-void Entrenador::asignarRutina(Cliente &cliente, string nombreRutina, string objetivoRutina) {
-    cliente.asignarRutina(nombreRutina, objetivoRutina);
-}
-
-string Entrenador::obtenerResumen() {
-    return "Entrenador: " + nombre +
-           "  Especialidad: " + especialidad +
-           "  Experiencia: " + to_string(aniosExperiencia) + " anios";
-}
-#endif //UNTITLED4_ENTRENADOR_H
+#endif // UNTITLED4_PROFESOR_H
