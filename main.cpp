f283f7a87d39cf17e4a9b5eb8b348e98395bf224
Erick26m <a01715444@tec.mx>
2026-06-03 18:35:07 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+/*
+
+En el main se prueban las clases principales del sistema.
+Primero se crean objetos de tipo Inscripcion, Curso, Estudiante y Profesor.
+Después, el profesor asigna cursos a los estudiantes.
+
+También se demuestra el uso de polimorfismo usando apuntadores de tipo Persona*.
+Gracias al método virtual obtenerResumen(), el programa ejecuta la versión
+correcta dependiendo del tipo real del objeto.
+
+Al final se libera la memoria con delete porque los objetos fueron creados
+dinámicamente con new.
+*/
 #include "estudiante.h"
 #include "profesor.h"
 #include <iostream>
