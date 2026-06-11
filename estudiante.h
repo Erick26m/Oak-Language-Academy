25d405b8678023bc14c36cad9e997ee1060833b5
Erick26m <a01715444@tec.mx>
2026-06-11 10:48:57 -0600

    Update estudiante.h

--- estudiante.h
+++ estudiante.h
@@ -1,24 +1,32 @@
-//
-// Created by munoz on 25/05/2026.
-//
-/* Clase Estudiante 
-Esta clase representa al estudiante registrado dentro de la academia.
-Relación con otras clases: 
-- Hereda de Persona. 
-- Tiene una Inscripcion. 
-- Tiene un Curso asignado.
-- Puede recibir un curso por medio de la clase Profesor.
-La clase almacena información propia del estudiante, como su promedio, su inscripción y el curso que se le asigna dentro del sistema. 
-También implementa el método obtenerResumen(), que viene de la clase Persona. 
-Esto permite que un Estudiante pueda ser tratado como Persona usando polimorfismo, pero mostrando su información específica como estudiante.
-*/
+#ifndef ESTUDIANTE_H
+#define ESTUDIANTE_H
+#include <iostream>
 #include <string>
 #include "persona.h"
 #include "inscripcion.h"
 #include "curso.h"
-#include <iostream>
 
 using namespace std;
+/*
+Clase Estudiante
+
+Esta clase representa al estudiante registrado dentro de la academia.
+Hereda de la clase Persona, por lo que reutiliza atributos como nombre,
+edad y matrícula.
+
+Relación con otras clases:
+- Hereda de Persona.
+- Tiene una Inscripcion.
+- Tiene un Curso asignado.
+- Puede recibir un curso por medio de la clase Profesor.
+
+La clase almacena información propia del estudiante, como su promedio,
+su inscripción y el curso que se le asigna dentro del sistema.
+
+También implementa el método obtenerResumen(), que viene de la clase Persona.
+Esto permite que un objeto Estudiante pueda ser tratado como Persona usando
+polimorfismo, pero mostrando su información específica como estudiante.
+*/
 
 class Estudiante : public Persona {
 private:
@@ -75,10 +83,11 @@ void Estudiante::asignarCurso(string nombreCurso, int duracionHoras) {
 
 string Estudiante::obtenerResumen() {
     return "Estudiante: " + nombre +
-           "  Edad: " + to_string(edad) +
-           "  Matricula: " + matricula +
-           "  Promedio: " + to_string(promedio) +
-           "  " + inscripcion.obtenerDescripcion() +
-           "  Curso asignado: " + curso.getNombre();
+           " | Edad: " + to_string(edad) +
+           " | Matricula: " + matricula +
+           " | Promedio: " + to_string(promedio) +
+           " | " + inscripcion.obtenerDescripcion() +
+           " | Curso asignado: " + curso.getNombre();
 }
-#endif //UNTITLED4_CLIENTE_H
+
+#endif
